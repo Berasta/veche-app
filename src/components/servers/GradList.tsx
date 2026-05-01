518bdef2e1790974e2d10ee5394a002098b36420
rLukoyanov <rulukoyanov@gmail.com>
2026-05-01 20:52:53 +0400

    fix: close drawer JSX structure

--- src/components/servers/GradList.tsx
+++ src/components/servers/GradList.tsx
@@ -182,6 +182,12 @@ export function GradList() {
                     </div>
                     <ServerMembers serverId={serverId} isOpen={true} onClose={() => setShowDrawer(false)} inline />
                   </div>
+                )}
+              </div>
+            </div>
+          </>
+        )}
+
         {showCreateServer && (
           <Portal>
             <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center" onClick={() => setShowCreateServer(false)}>
