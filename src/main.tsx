c18eff237d6f52fc76a22607c7e1a928a115f26d
rLukoyanov <rulukoyanov@gmail.com>
2026-05-01 19:27:42 +0400

    add TanStack Query for cached channel/server queries

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { App } from "./App";
 
 import { store } from "./store";
 
 import "./styles/index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30_000,
+      retry: 1,
+    },
+  },
+});
+
 // @ts-ignore
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 );
